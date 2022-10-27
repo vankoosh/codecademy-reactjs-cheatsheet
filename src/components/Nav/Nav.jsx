5dbae000a9465bba570cf438e5c9e0300c68b3ab
vankoosh <vankoosh@gmail.com>
2022-10-27 18:32:36 +0000

    again

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -74,7 +74,7 @@ export default function Nav() {
               setTopic([
                 {
                   id: ".map() in JSX",
-                  src: "assets/basics/.map()inJSX.jpg",
+                  src: "assets/basics/map()inJSX.jpg",
                 },
                 {
                   id: "if...else in JSX",
